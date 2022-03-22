bc1de3c0c09a48f559cecbbca51d827523a659e9
sanghun.gwon <sanghun.gwon@nhn.com>
2022-03-22 16:52:36 +0900

    simple hello batch

--- src/main/java/com/example/inflearnbatch/InflearnBatchApplication.java
+++ src/main/java/com/example/inflearnbatch/InflearnBatchApplication.java
@@ -1,9 +1,14 @@
 package com.example.inflearnbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
+// 스프링 배치가 작동하기 위해 선언해야 하는 어노테이션
+//      4개의 설정 클래스를 실행시키며 스프링 배치의 모든 초기화 및 실행 구성이 이루어짐(BatchAutoConfiguration, SimpleBatchConfiguration, BatchConfigurerConfiguration)
+//      스프링 부트 배치의 자동 설정 클래스가 실행됨으로 빈으로 등록된 모든 잡을 검색해서 초기화와 동시해 잡을 수행하도록 구성
 public class InflearnBatchApplication {
 
     public static void main(String[] args) {
